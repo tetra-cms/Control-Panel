0b14876207f4a97bdb0b6c16fbc87465dedacef5
fet1sov <august9222@gmail.com>
2026-01-22 03:22:48 +0500

    feat(grid-list): finish the grid list component (#2)
    added the grid list components which shows the certain information in table
    
    Refs: #2

--- components/gridlist/GridList.vue
+++ components/gridlist/GridList.vue
@@ -1,28 +1,40 @@
 <script lang="ts" setup>
-import type GridListColumn from '~/types/gridlist/GridListColumn';
+import type GridListItem from '~/types/gridlist/GridListItem';
+
+interface RowData {
+    [key: string]: string
+}
 
 const props = defineProps<{
-    title: string,
-    columns: Array<GridListColumn>,
-    items: Array<string>
+    items: RowData[]
 }>();
+
+const columns = props.items.length > 0 ? Object.keys(props.items[0]) : [];
 </script>
 
 <template>
     <div class="w-[50%] rounded-[10px] bg-secondary-primary p-[20px]">
-        <p class="text-primary-primary">{{ title }}</p>
-        <table>
+        <table class="w-full text-center">
             <thead>
                 <tr>
-                    <th v-for="column in columns" class="text-left pb-[10px]">
-                        {{ $t(column.title) }}
+                    <th 
+                        v-for="column in columns" 
+                        :key="column"
+                        class="pb-[10px]">
+                        {{ $t(column) }}
                     </th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="item in items" class="border-t border-secondary-secondary">
-                    <td v-for="column in columns" class="py-[10px]">
-                        {{ item }}
+                <tr 
+                    v-for="(item, index) in items" 
+                    :key="index"
+                    class="border-t border-secondary-secondary">
+                    <td 
+                        v-for="column in columns" 
+                        :key="column"
+                        class="py-[10px]">
+                        {{ item[column] }}
                     </td>
                 </tr>
             </tbody>
