b7e11831c02b7e4fae50db8546bb531cfaac1f74
fet1sov <august9222@gmail.com>
2026-07-22 11:44:44 +0500

    chore(upgrade): upgrade dependencies to a new version

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,35 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    theme: {
-        extend: {
-            colors: {
-                'primary': {
-                    'primary': '#000000',
-                    'secondary': '#252525'
-                },
-                'secondary': {
-                    'primary': '#FFFFFF',
-                    'light': '#eeeeee',
-                    'darklight': '#e7e7e7',
-                    'wrapper': {
-                        'dark': '#000000',
-                        'invert': '#ffffff',
-                        'light': '#b9b9b9'
-                    }
-                }
-            },
-            
+  content: [
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './composables/**/*.{js,ts}',
+    './app.vue',
+    './error.vue'
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          primary: '#000000',
+          secondary: '#252525'
+        },
+        secondary: {
+          primary: '#FFFFFF',
+          light: '#eeeeee',
+          darklight: '#e7e7e7',
+          wrapper: {
+            dark: '#000000',
+            invert: '#ffffff',
+            light: '#b9b9b9'
+          }
         }
-    },
-    variants: {},
-    plugins: [],
-    purge: {}
+      }
+    }
+  },
+
+  plugins: []
 }
